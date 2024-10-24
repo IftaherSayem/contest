c59b8c109af295b2c8f7e6bc5809bbb3ec9e0cf3
IftaherSayem <iftahersayem205@gmail.com>
2024-10-24 22:05:20 +0600

    Create 2033A.cpp

---
+++ DIV3/2033A.cpp
@@ -0,0 +1,48 @@
+/*
+========================================
++          IFTAHER HOSSEN SAYEM        +
++            IIUC,Dept. of CSE         +
+========================================
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+const int m=1e9 + 7;
+void test()
+{
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+}
+bool chk(int a)
+{
+    if(a==2 || a==3 || a==5) return true;
+    for(int i=2; i*i<=a; i++)
+    {
+        if(a%i==0)
+        {
+            return false;
+        }
+    }
+    return true;
+}
+int main()
+{
+    //test();
+    ios_base::sync_with_stdio(false),cin.tie(NULL),cout.tie(NULL);
+    ll n;
+    cin >> n;
+    while(n--)
+    {
+        ll a,b,c,d;
+        cin >> a;
+        if(a%2==0)
+        {
+           cout << "Sakurako" << endl;
+        }
+        else
+        {
+           cout << "Kosuke" << endl;
+        }
+    }
+}
