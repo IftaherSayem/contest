b930568f4e8ae9bda7e3574755a2b6a51cae3ebc
IftaherSayem <iftahersayem205@gmail.com>
2024-11-18 08:26:28 +0600

    Create 2037C.cpp

---
+++ DIV3/2037C.cpp
@@ -0,0 +1,73 @@
+/*
+========================================
++          IFTAHER HOSSEN SAYEM        +
++            IIUC,Dept. of CSE         +
+========================================
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+#define el "\n"
+#define yes cout << "YES" << el;
+#define no cout << "NO" << el;
+const int m=1e9 + 7;
+void test()
+{
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+}
+bool chk(int a)
+{
+    if(a==2 || a==3 || a==5) return true;
+    for(int i=2; i*i<=a; i++)
+    {
+        if(a%i==0)
+        {
+            return false;
+        }
+    }
+    return true;
+}
+
+ll power(ll x, ll y)
+{
+    ll val = 1;
+
+    while (y > 0)
+    {
+        if (y % 2 != 0)
+            val = val * x;
+        y = y / 2;
+        x = x * x;
+    }
+    return val;
+}
+
+int main()
+{
+    //test();
+    ios_base::sync_with_stdio(false),cin.tie(NULL),cout.tie(NULL);
+    ll n;
+    cin >> n;
+    while (n--)
+    {
+        int a;
+        cin >> a;
+        if(a<=4)
+        {
+            cout << -1 << el;
+            continue;
+        }
+        for (int j=7;j<=a;j+=2)
+        {
+            cout << j << " ";
+        }
+        cout << "3 1 5 4 2 " << el;
+        for (int j=6;j<=a;j+=2)
+        {
+            cout << j << " ";
+        }
+        cout << el;
+    }
+}
