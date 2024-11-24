a2b4cbacd2280f640f1de51af489813b2072a79e
IftaherSayem <iftahersayem205@gmail.com>
2024-11-24 18:09:52 +0600

    Create 2041A.cpp

---
+++ 2024 ICPC Asia Taichung Regional Contest (Unrated, Online Mirror, ICPC Rules, Preferably Teams)/2041A.cpp
@@ -0,0 +1,70 @@
+/*
+========================================
++          IFTAHER HOSSEN SAYEM        +
++            IIUC,Dept. of CSE         +
+========================================
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+#define FAST_IN_OUT ios_base::sync_with_stdio(false),cin.tie(NULL),cout.tie(NULL);
+#define loop(a,k) for (int i=a;i<k;i++)
+#define full(ar) (ar.begin(),ar.end())
+#define yes cout << "YES" << el;
+#define no cout << "NO" << el;
+#define pb push_back
+#define ll long long
+#define el "\n"
+const int m=1e9 + 7;
+void test()
+{
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+}
+bool chk(int a)
+{
+    if(a==2 || a==3 || a==5) return true;
+    for(int i=2; i*i<=a; i++)
+    {
+        if(a%i==0)
+        {
+            return false;
+        }
+    }
+    return true;
+}
+
+ll power(ll x, ll y)
+{
+    ll val = 1;
+
+    while (y > 0)
+    {
+        if (y % 2 != 0)
+            val = val * x;
+        y = y / 2;
+        x = x * x;
+    }
+    return val;
+}
+
+int main()
+{
+    //test();
+    FAST_IN_OUT
+    //ll n;
+    //cin >> n;
+    //while (n--)
+    //{
+    //ll a,b;
+    //cin >> a >> b;
+    int a,b,c,d;
+    cin >> a >> b >> c >> d;
+    int sum=a+b+c+d;
+    if(sum==10) cout << 5 << el;
+    else if(sum==14) cout << 1 << el;
+    else if(sum==13) cout << 2 << el;
+    else if(sum==12) cout << 3 << el;
+    else if(sum==11) cout << 4 << el;
+    //}
+}
