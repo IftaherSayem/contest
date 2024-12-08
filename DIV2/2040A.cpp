cddff26e7900cf3e1b5bbe3e79316a5eae4805aa
IftaherSayem <iftahersayem205@gmail.com>
2024-12-08 23:11:24 +0600

    Create 2040A.cpp

---
+++ DIV2/2040A.cpp
@@ -0,0 +1,103 @@
+/*
+========================================
++          IFTAHER HOSSEN SAYEM        +
++            IIUC,Dept. of CSE         +
+========================================
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+#define FAST_IN_OUT ios_base::sync_with_stdio(false),cin.tie(NULL),cout.tie(NULL);
+#define loop(a,k) for (int i=a;i<k;i++)
+#define full(ar) (ar.begin(),ar.end())
+#define lcm(a,b) (a*b)/__gcd(a,b)
+#define yes cout << "YES" << el;
+#define no cout << "NO" << el;
+#define gcd(a,b) __gcd(a,b)
+#define pb push_back
+#define ll long long
+#define el "\n"
+const int m=1e9 + 7;
+void test()
+{
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+}
+bool chk(int a)
+{
+    if(a==2 || a==3 || a==5) return true;
+    for(int i=2; i*i<=a; i++)
+    {
+        if(a%i==0)
+        {
+            return false;
+        }
+    }
+    return true;
+}
+
+ll power(ll x, ll y)
+{
+    ll val = 1;
+
+    while (y > 0)
+    {
+        if (y % 2 != 0)
+            val = val * x;
+        y = y / 2;
+        x = x * x;
+    }
+    return val;
+}
+
+int main()
+{
+    //test();
+    FAST_IN_OUT
+    ll n;
+    cin >> n;
+    while(n--)
+    {
+        int a,b;
+        cin >> a >> b;
+        vector<int>ar(a);
+        for (int i=0;i<a;i++)
+        {
+            cin >> ar[i];
+        }
+        if(a==1)
+        {
+            
+        }
+        int store=-1;
+        //bool chk=false;
+        int co=0;
+        bool yeah=false;
+        for (int i=0;i<a;i++)
+        {
+            bool chk=false;
+            for (int j=0;j<a;j++)
+            {
+                int ab=abs(ar[i]-ar[j]);
+                if(i==j) continue;
+                if(ab%b==0)
+                {
+                   chk=true;
+                   break;
+                }
+            }
+            if(!chk)
+            {
+                yeah=true;
+                store=i+1;
+                break;
+            }
+        }
+        if(yeah)
+        {
+            yes
+            cout << store << el;
+        }
+        else no
+    }
+}
