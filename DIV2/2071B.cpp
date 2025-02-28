ed3e276661706723f093813bf8afe4a5c9884609
IftaherSayem <iftahersayem205@gmail.com>
2025-02-28 22:43:03 +0600

    Create 2071B.cpp

---
+++ DIV2/2071B.cpp
@@ -0,0 +1,110 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define FAST_IN_OUT ios_base::sync_with_stdio(false),cin.tie(NULL),cout.tie(NULL);
+#define loop(a,k) for (int i=a;i<k;i++)
+#define lcm(a,b) (a*b)/__gcd(a,b)
+#define yes cout << "YES" << el;
+#define no cout << "NO" << el;
+#define gcd(a,b) __gcd(a,b)
+#define ll long long
+#define el "\n"
+#define sp " "
+#define mod 998244353
+const int m=1e9 + 7;
+void test()
+{
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+}
+bool chk(int a)
+{
+    if(a==2 || a==3 || a==5) return true;
+    for(int i=2; i*i<=a; i++)
+    {
+        if(a%i==0)
+        {
+            return false;
+        }
+    }
+    return true;
+}
+
+int power(int x, int y)
+{
+    int val = 1;
+
+    while (y > 0)
+    {
+        if (y % 2 != 0)
+            val = val * x;
+        y = y / 2;
+        x = x * x;
+    }
+    return val;
+}
+
+void binary_modular_exponentiation(ll  w,ll x,ll y)
+{
+    ll ans=1;
+    while(x)
+    {
+        if(x%2==1)
+        {
+            ans=(ans*w);
+            x--;
+        }
+        else
+        {
+            w=(w*w);
+            x/=2;
+        }
+    }
+    cout << y%ans << el;
+}
+
+int Euclid(int x, int y)
+{
+    while(y!=0)
+    {
+        int rem=x%y;
+        x=y;
+        y=rem;
+    }
+    return x;
+}
+bool prfcsq(ll a)
+{
+    ll squ = sqrt(a);
+    return (squ * squ == a);
+}
+int main()
+{
+    //test();
+    FAST_IN_OUT
+    ll n;
+    cin >> n;
+    while(n--)
+    {
+        int a,b,c;
+        cin >> a;
+        ll an=1LL*a*(a+1)/2;
+        if(prfcsq(an))
+        {
+            cout << -1 << el;
+            continue;
+        }
+        vector<int>rs(a);
+        for (int i=0;i<a;i++)
+        {
+            rs[i]=i+1;
+        }
+        vector<int> ar={1,8,49,288,1681,9800,57121,332928};
+        for (int i=0;i<8;i++)
+        {
+            if(ar[i]>a) break;
+            swap(rs[ar[i]],rs[ar[i]-1]);
+        }
+        for (int i=0;i<a;i++) cout << rs[i] << sp;
+        cout << el;
+    }
+}
