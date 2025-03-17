a2e76d5786931b198985d502d45fff28d18edd73
IftaherSayem <iftahersayem205@gmail.com>
2025-03-17 21:56:02 +0600

    Create 2075A.cpp

---
+++ DIV2/2075A.cpp
@@ -0,0 +1,118 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define FAST_IN_OUT ios_base::sync_with_stdio(false),cin.tie(NULL),cout.tie(NULL);
+#define loop(a,k) for (int i=a;i<k;i++)
+#define lcm(a,b) (a*b)/__gcd(a,b)
+#define yes cout << "YES" << el;
+#define no cout << "NO" << el;
+#define gcd(a,b) __gcd(a,b)
+#define ll long long
+#define el "\n"
+#define sp " "
+#define MOD 998244353
+const int m=1e9 + 7;
+void test()
+{
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+}
+bool chk(int a)
+{
+    if(a==2 || a==3 || a==5) return true;
+    for(int i=2; i*i<=a; i++)
+    {
+        if(a%i==0)
+        {
+            return false;
+        }
+    }
+    return true;
+}
+
+int power(int x, int y)
+{
+    int val = 1;
+
+    while (y > 0)
+    {
+        if (y % 2 != 0)
+            val = val * x;
+        y = y / 2;
+        x = x * x;
+    }
+    return val;
+}
+
+void binary_modular_exponentiation(ll  w,ll x,ll y)
+{
+    ll ans=1;
+    while(x)
+    {
+        if(x%2==1)
+        {
+            ans=(ans*w);
+            x--;
+        }
+        else
+        {
+            w=(w*w);
+            x/=2;
+        }
+    }
+    cout << y%ans << el;
+}
+
+int Euclid(int x, int y)
+{
+    while(y!=0)
+    {
+        int rem=x%y;
+        x=y;
+        y=rem;
+    }
+    return x;
+}
+bool prfcsq(ll a)
+{
+    ll squ = sqrt(a);
+    return (squ * squ == a);
+}
+
+int nxpr(int a)
+{
+    for (int i=a;;i++)
+    {
+        bool chk=true;
+        for (int j=2;j*j<=i;j++)
+        {
+            if(i%j==0)
+            {
+                chk=false;
+                break;
+            }
+        }
+        if(chk) return i;
+    }
+}
+
+int main()
+{
+    //test();
+    FAST_IN_OUT
+    ll n;
+    cin >> n;
+    while(n--)
+    {
+        ll a,b,c,d;
+        string s;
+        cin >> a >> b;
+        int co=0;
+        a-=b;
+        co++;
+        co+=(a/(b-1));
+        int baki=(a/(b-1))*(b-1);
+        a-=baki;
+        if(a==0) cout << co << el;
+        else cout << co+1 << el;
+    }
+}
